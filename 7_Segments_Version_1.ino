0a4089a932091fd29c1bcbaaad5212c6fe2afd33
MichaelaLuna <116026170+MichaelaLuna@users.noreply.github.com>
2023-03-06 14:44:11 -0500

    Add files via upload

---
+++ 7_Segments_Version_1.ino
@@ -0,0 +1,118 @@
+
+void setup() { // put your setup code here, to run once:
+  
+pinMode(5,OUTPUT); // E #1
+pinMode(6,OUTPUT); // D #2
+pinMode(7,OUTPUT); // C #4
+pinMode(8,OUTPUT); // DP
+pinMode(9,OUTPUT); // B #6
+pinMode(10,OUTPUT); // A #7
+pinMode(11,OUTPUT); // F #9
+pinMode(12,OUTPUT); // G #10
+
+}
+
+void loop() { // put your main code here, to run repeatedly:
+
+
+digitalWrite(5,HIGH); // E #1
+digitalWrite(6,HIGH); // D #2
+digitalWrite(7,HIGH); // C #4
+digitalWrite(8,HIGH); // DP 
+digitalWrite(9,HIGH); // B #6
+digitalWrite(10,HIGH); // A #7
+digitalWrite(11,HIGH); // F #9 
+digitalWrite(12,LOW); // G #10
+// Start of 1
+delay(1000); 
+digitalWrite(5,LOW); // E #1
+digitalWrite(6,LOW); // D #2
+digitalWrite(7,HIGH); // C #4 
+digitalWrite(8,HIGH); // DP
+digitalWrite(9,HIGH); // B #6
+digitalWrite(10,LOW); // A #7
+digitalWrite(11,LOW); // F #9
+digitalWrite(12,LOW); // G #10
+//Start of 2
+delay(1000); 
+digitalWrite(5,HIGH); // E #1
+digitalWrite(6,HIGH); // D #2
+digitalWrite(7,LOW); // C #4
+digitalWrite(8,HIGH); // DP
+digitalWrite(9,HIGH); // B #6
+digitalWrite(10,HIGH); // A #7
+digitalWrite(11,LOW); // F #9
+digitalWrite(12,HIGH); // G #10
+//Start of 3
+delay(1000); 
+digitalWrite(5,LOW); // E #1
+digitalWrite(6,HIGH); // D #2
+digitalWrite(7,HIGH); // C #4
+digitalWrite(8,HIGH); // DP
+digitalWrite(9,HIGH); // B #6
+digitalWrite(10,HIGH); // A #7
+digitalWrite(11,LOW); // F #9
+digitalWrite(12,HIGH); // G #10
+//Start of 4
+delay(1000);
+digitalWrite(5,LOW); // E #1
+digitalWrite(6,LOW); // D #2
+digitalWrite(7,HIGH); // C #4
+digitalWrite(8,HIGH); // DP
+digitalWrite(9,HIGH); // B #6
+digitalWrite(10,LOW); // A #7
+digitalWrite(11,HIGH); // F #9
+digitalWrite(12,HIGH); // G #10
+//Start of 5
+delay(1000);
+digitalWrite(5,LOW); // E #1
+digitalWrite(6,HIGH); // D #2
+digitalWrite(7,HIGH); // C #4
+digitalWrite(8,HIGH); // DP
+digitalWrite(9,LOW); // B #6 
+digitalWrite(10,HIGH); // A #7
+digitalWrite(11,HIGH); // F #9
+digitalWrite(12,HIGH); // G #10
+//Start of 6
+delay(1000);
+digitalWrite(5,HIGH); // E #1
+digitalWrite(6,HIGH); // D #2
+digitalWrite(7,HIGH); // C #4
+digitalWrite(8,HIGH); // DP 
+digitalWrite(9,LOW); // B #6
+digitalWrite(10,LOW); // A #7
+digitalWrite(11,HIGH); // F #9
+digitalWrite(12,HIGH); // G #10
+//Start of 7
+delay(1000);
+digitalWrite(5,LOW); // E #1
+digitalWrite(6,LOW); // D #2
+digitalWrite(7,HIGH); // C #4
+digitalWrite(8,HIGH); // DP
+digitalWrite(9,HIGH); // B #6
+digitalWrite(10,HIGH); // A #7
+digitalWrite(11,LOW); // F #9
+digitalWrite(12,LOW); // G #10
+//Start of 8
+delay(1000);
+digitalWrite(5,HIGH); // E #1
+digitalWrite(6,HIGH); // D #2
+digitalWrite(7,HIGH); // C #4
+digitalWrite(8,HIGH); // DP
+digitalWrite(9,HIGH); // B #6
+digitalWrite(10,HIGH); // A #7
+digitalWrite(11,HIGH); // F #9
+digitalWrite(12,HIGH); // G #10
+//Start of 9
+delay(1000);
+digitalWrite(5,LOW); // E #1
+digitalWrite(6,LOW); // D #2 
+digitalWrite(7,HIGH); // C #4
+digitalWrite(8,HIGH); // DP
+digitalWrite(9,HIGH); // B #6
+digitalWrite(10,HIGH); // A #7
+digitalWrite(11,HIGH); // F #9
+digitalWrite(12,HIGH); // G #10
+delay(1000);
+
+}
