219eb4a2f70b5e8a83bd7dbbd25363db7cb94b8b
MichaelaLuna <116026170+MichaelaLuna@users.noreply.github.com>
2023-03-07 22:58:43 -0500

    Update 4 Digit  7 Segment + Shift Register.ino

--- 4 Digit  7 Segment + Shift Register.ino
+++ 4 Digit  7 Segment + Shift Register.ino
@@ -2,29 +2,27 @@
 int LatchPin = 19;  // defines pin for the latch
 int ShiftPin = 18;  // defines pin for shift 
 int DataInput = 10; // defines pin for data input 
-int digPins[4] = {A0,A1,A2,12}; // has four dedicated common pins to control each digit separately */
+int digPins[4] = {A0,A1,12,13}; // has four dedicated common pins to control each digit separately */
 /*
-int D1 = 12; //12
-int D2 = A2; //A2
+int D1 = 13; //12
+int D2 = 12; //12
 int D3 = A1; //A1
 int D4 = A0; //A0
 */
 
 
-byte digits[10] = {  
+const byte digits[10] = {  
   // defines an array of bytes that represent the pattern to light up each segment of a digit.
-
-  
-  0B11110110,  // 0 FABCGED
-  0B00110000,  // 1
-  0B01101110,  // 2
-  0B01111010,  // 3
-  0B01111000,  // 4
-  0B11011100,  // 5
-  0B11011110,  // 6
-  0B10101000,  // 7
-  0B11111110,  // 8
-  0B11111100,  // 9
+  B11101111,  // 0 afbgc-de
+  B00101100,  // 1
+  B10110111,  // 2
+  B10111110,  // 3
+  B01111100,  // 4
+  B11011110,  // 5
+  B11011111,  // 6
+  B10101100,  // 7
+  B11111111,  // 8
+  B11111110,  // 9
 };
 
 
@@ -34,115 +32,52 @@ void setup() { // code run once
   pinMode (DataInput, OUTPUT);
   pinMode (A0, OUTPUT);
   pinMode (A1, OUTPUT);
-  pinMode (A2, OUTPUT);
-  pinMode (12, OUTPUT); 
-  Serial.begin(9600);
+  pinMode (12, OUTPUT);
+  pinMode (13, OUTPUT); 
 }
 
 void loop () { // code runs in a loop 
 
- delay(3);
-  digitalWrite (12, LOW);
-  digitalWrite (A2, HIGH);
-  digitalWrite (A1, HIGH);
-  digitalWrite (A0, HIGH);
-  digitalWrite (LatchPin, LOW);
-  shiftOut(DataInput, ShiftPin, MSBFIRST, digits[0]/*0B10110110*/);
-  digitalWrite (LatchPin, HIGH); 
- 
- delay(3);
-  digitalWrite (12, HIGH);
-  digitalWrite (A2, LOW);
-  digitalWrite (A1, HIGH);
-  digitalWrite (A0, HIGH);
-  digitalWrite (LatchPin, LOW);
-  shiftOut(DataInput, ShiftPin, MSBFIRST, digits[1]/*0B11101110*/ );
-  digitalWrite (LatchPin, HIGH); 
 
- delay(3);
- 
+  digitalWrite (13, LOW);
   digitalWrite (12, HIGH);
-  digitalWrite (A2, HIGH);
+  digitalWrite (A1, HIGH);
+  digitalWrite (A0, HIGH);
+  digitalWrite (LatchPin, LOW);
+  shiftOut(DataInput, ShiftPin, MSBFIRST,digits[2] );
+  digitalWrite (LatchPin, HIGH); 
+  delay(5);
+ 
+  
+
+ 
+  digitalWrite (13, HIGH);
+  digitalWrite (12, LOW);
+  digitalWrite (A1, HIGH);
+  digitalWrite (A0, HIGH);
+  digitalWrite (LatchPin, LOW);
+  shiftOut(DataInput, ShiftPin, MSBFIRST, digits[4] );
+  digitalWrite (LatchPin, HIGH);
+  delay(5);
+  
+ 
+  digitalWrite (13, HIGH);
+  digitalWrite (12, HIGH);
   digitalWrite (A1, LOW);
   digitalWrite (A0, HIGH);
   digitalWrite (LatchPin, LOW);
-  shiftOut(DataInput, ShiftPin, MSBFIRST,digits[2]/* 0B11101110*/);
+  shiftOut(DataInput, ShiftPin, MSBFIRST,digits[6]);
   digitalWrite (LatchPin, HIGH); 
+  delay(5);
 
- delay(3);
-
- 
+  digitalWrite (13, HIGH);
   digitalWrite (12, HIGH);
-  digitalWrite (A2, HIGH);
   digitalWrite (A1, HIGH);
   digitalWrite (A0, LOW);
   digitalWrite (LatchPin, LOW);
-  shiftOut(DataInput, ShiftPin, MSBFIRST, digits[3] /*0B10110110*/ );
+  shiftOut(DataInput, ShiftPin, MSBFIRST, digits[8]);
   digitalWrite (LatchPin, HIGH);
-
-
- delay(3);
+  delay(5);
 
 
 }
-
-
-
-
-/*
-
-int ClockPin = 9;  // defines pin for the latch
-int DataInput = 8; // defines pin for data input 
-int digPins[4] = {5,4,3,2}; // has four dedicated common pins to control each digit separately 
-
-void setup() { // code run once 
-  pinMode (ClockPin, OUTPUT);
-  pinMode (DataInput, OUTPUT);
-  pinMode (5, OUTPUT);
-  pinMode (4, OUTPUT);
-  pinMode (3, OUTPUT);
-  pinMode (2, OUTPUT); 
-  Serial.begin(9600);
-}
-
-void loop () { // code runs in a loop 
- 
-  digitalWrite (5, LOW);
-  digitalWrite (4, HIGH);
-  digitalWrite (3, HIGH);
-  digitalWrite (2, HIGH);
-  digitalWrite (ClockPin, LOW);
-  shiftOut(DataInput, ClockPin, MSBFIRST,0B00101000);
-  digitalWrite (ClockPin, HIGH); 
-
-
-
-  digitalWrite (5, HIGH);
-  digitalWrite (4, LOW);
-  digitalWrite (3, HIGH);
-  digitalWrite (2, HIGH);
-  digitalWrite (ClockPin, LOW);
-  shiftOut(DataInput, ClockPin, MSBFIRST, 0B10110110 );
-  digitalWrite (ClockPin, HIGH); 
- 
-
-  digitalWrite (5, HIGH);
-  digitalWrite (4, HIGH);
-  digitalWrite (3, LOW);
-  digitalWrite (2, HIGH);
-  digitalWrite (ClockPin, LOW);
-  shiftOut(DataInput, ClockPin, MSBFIRST, 0B10111100);
-  digitalWrite (ClockPin, HIGH); 
-
- 
-  digitalWrite (5, HIGH);
-  digitalWrite (4, HIGH);
-  digitalWrite (3, HIGH);
-  digitalWrite (2, LOW);
-  digitalWrite (ClockPin, LOW);
-  shiftOut(DataInput, ClockPin, MSBFIRST,0B01111000 );
-  digitalWrite (ClockPin, HIGH); 
-
- 
-}
-*/
