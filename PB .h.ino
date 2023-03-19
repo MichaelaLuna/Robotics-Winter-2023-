d5bba0c7fb13898663e6ef2dd4831e2a089f7474
MichaelaLuna <116026170+MichaelaLuna@users.noreply.github.com>
2023-03-18 22:00:21 -0400

    Create PB .h.ino

---
+++ PB .h.ino
@@ -0,0 +1,18 @@
+/*--- This is the code for the header ---*/
+
+#ifndef PUSHBUTTON_H // If not define
+#define PUSHBUTTON_H //Define
+
+class PushButton { // Set a name for the class
+  
+  private: // The encapsulation
+     int pin; // Initialize the pin
+     int state; // Initialize the state
+
+  public: // Can be accces outside the class
+    PushButton(int buttonPin) { // Constructor I'm gonna use
+    bool pressed() { // When PB is pressed
+    
+}; // End
+
+#endif // End of if not defined
